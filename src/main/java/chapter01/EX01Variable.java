28ca4ee5d424379fb441f1ca36fac2b31a2c4b63
Son-Sumin <goldkitty007@gmail.com>
2022-10-20 10:58:49 +0900

    chapter01 #1

---
+++ src/main/java/chapter01/EX01Variable.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class EX01Variable {
+
+	public static void main(String[] args) {
+		int i = 1000;
+		String s = "Hello World";  // = new String("Hello World") string은 자주 사용하기에 java에서 제공하는 서비스
+		
+		System.out.println(i);
+		System.out.println(s);
+	}
+
+}
