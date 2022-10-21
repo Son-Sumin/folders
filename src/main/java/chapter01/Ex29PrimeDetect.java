3aadc27ad6095ad727246606a7c50b2c9b840eb2
Son-Sumin <goldkitty007@gmail.com>
2022-10-21 13:48:40 +0900

    제어문 #2

---
+++ src/main/java/chapter01/Ex29PrimeDetect.java
@@ -0,0 +1,20 @@
+package chapter01;
+
+public class Ex29PrimeDetect {
+
+	public static void main(String[] args) {
+		// 2 ~ 1000까지 소수를 화면에 출력하세요.
+		
+		int num = 5;		
+		boolean isPrime = true;
+
+		
+		
+		if(isPrime) {
+			System.out.println(num);
+			
+		}
+
+	}
+
+}
