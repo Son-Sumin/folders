aff646a0fc6579a5f6ffd312b38ec205a4be0c77
Son-Sumin <goldkitty007@gmail.com>
2022-10-21 14:11:52 +0900

    project 구조 변경

---
+++ chapter01/src/main/java/chapter01/Ex30Gugudan2.java
@@ -0,0 +1,21 @@
+package chapter01;
+
+public class Ex30Gugudan2 {
+
+	public static void main(String[] args) {
+		// System.out.println("1 X 1 = 1");
+//		System.out.print("1 X 1 = 1");
+//		System.out.print("\t");
+//		System.out.print("2 X 1 = 1");
+//		System.out.print("\t");
+//		
+//		System.out.print("\n");
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.print("");
+		}
+		
+
+	}
+
+}
