e9ac4bec3c5d80d6aada8c8a1a57dc2cd519ecf1
Son-Sumin <goldkitty007@gmail.com>
2022-10-23 16:11:04 +0900

    practice01 #05

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,9 +3,11 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
-//		for(int i = 1; i < 100; i++) {
-//			
-//		}
+		for(int i = 1; i < 100; i++) {
+			char c = text.charAt(i);
+			if(c = "3" || c = "6" || c = "9")
+				System.out.println("짝");
+		}
 		int i = 36;
 		int count = 0;
 		
