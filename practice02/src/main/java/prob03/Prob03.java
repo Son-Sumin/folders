eef8d33f5c0b728f39cc6ffd670932f7800e372a
Son-Sumin <goldkitty007@gmail.com>
2022-10-24 17:25:05 +0900

    practice02 #3

--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -1,21 +1,32 @@
-//package prob03;
-//
-//public class Prob03 {
-//	public static void main(String args[]) {
-//		char[] charArray = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
-//
-////		// 원래 배열 원소 출력
-////		printCharArray(charArray);
-////
-////		// 공백 문자 바꾸기
-////		replaceSpace(charArray);
-////
-////		// 수정된 배열 원소 출력
-////		printCharArray(charArray);
-////	}
-////	
-////	public static void printCharArray(char[] cs) {
-////		System.out.println(cs);
-////	//공백문자 함수 만들고 공백을 ,로 바꾸기
-////
-////}
+package prob03;
+
+public class Prob03 {
+	public static void main(String args[]) {
+		char[] charArray = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
+
+		// 원래 배열 원소 출력
+		printCharArray(charArray);
+
+		// 공백 문자 바꾸기
+		replaceSpace(charArray);	
+
+		// 수정된 배열 원소 출력
+		// printCharArray(charArray);
+	}
+	
+	public static void replaceSpace(char[] blank) {
+		for (int i = 0; i < blank.length; i++) {
+			char b  = blank[i];
+			if(b == ' ') {
+				b = ',';
+			}
+			System.out.print(b);
+		}
+		System.out.print("\n");
+	}
+	
+	public static void printCharArray(char[] cs) {
+		System.out.println(cs);
+	}
+	
+}
