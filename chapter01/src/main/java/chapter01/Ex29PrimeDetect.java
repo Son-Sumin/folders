04d1205db96872b7c427bd66208191386ceaeabf
Son-Sumin <goldkitty007@gmail.com>
2022-10-25 12:37:06 +0900

    PrimeDetect

--- chapter01/src/main/java/chapter01/Ex29PrimeDetect.java
+++ chapter01/src/main/java/chapter01/Ex29PrimeDetect.java
@@ -4,23 +4,21 @@ public class Ex29PrimeDetect {
 
 	public static void main(String[] args) {
 		// 2 ~ 1000까지 소수를 화면에 출력하세요.
-		
-		int num = 5;		
+
 		boolean isPrime = true;
-		
-		if(isPrime) {
 
-			for (int i = 2; i < num; i++) {
-//				 num % i != 0;
+		for (int i = 2; i <= 1000; i++) {
+			for (int j = 2; i > j; j++) {
+				if (i % j != 0) {
+					isPrime = true;
+				} else {
+					isPrime = false;
+					//break;
+				}
 			}
-			num += 1;
-
-			for (int i = 2; i < num; i++);
-			//num % i == 0;
-			
-			System.out.println(num);
-		} else {
-			System.out.println(num + "은 소수가 아닙니다.");
+			if (isPrime) {
+				System.out.println(i);
+			}
 		}
 
 	}
