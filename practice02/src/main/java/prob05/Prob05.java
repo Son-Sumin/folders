5d672418876a56998a1749d722d4fbb5697fad69
Son-Sumin <goldkitty007@gmail.com>
2022-10-25 10:42:39 +0900

    코드작성

--- practice02/src/main/java/prob05/Prob05.java
+++ practice02/src/main/java/prob05/Prob05.java
@@ -14,35 +14,29 @@ public class Prob05 {
 			int correctNumber = random.nextInt(100) + 1;
 
 			System.out.println("수를 결정하였습니다. 맞추어 보세요: " + correctNumber + "\n1-100");
+			System.out.print("1>>");
 
-			while (true) {
-				System.out.print("1>>");
-				int answer = scanner.nextInt();
+			int answer = scanner.nextInt();
 
-				for (int i = 1; i <= 100; i++) {
+			for (int i = 1; i <= 100; i++) {
 
-					if (answer == correctNumber) {
-						break;
-					} else if (answer > correctNumber) {
-						System.out.print("더 낮게\n" + "1-" + answer);
-					} else {
-						System.out.print("더 높게\n" + answer + "-100");
-					}
+				if (answer == correctNumber) {
+					break;
+				} else if (answer > correctNumber) {
+					System.out.print("더 낮게\n" + "1-" + answer);
+				} else {
+					System.out.print("더 높게\n" + answer + "-100");
+				}
 
-					// 새 게임 여부 확인하기
-			System.out.print("다시 하겠습니까(y/n)>>");
-			//String answer = scanner.next();
-			if("y".equals(answer) == false) {
-				break;
-			
-			}
+				// 새 게임 여부 확인하기
+				System.out.print("다시 하겠습니까(y/n)>>");
+				// String answer = scanner.next();
+				if ("y".equals(answer) == false) {
+					break;
 
 				}
-			
 			}
 		}
-		//scanner.close();
-		
-	
-}
+		//ㄴ scanner.close();
+	}
 }
