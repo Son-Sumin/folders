3a6e671d3ebdf08a450efbf4fd9d1ee51639d19f
Son-Sumin <goldkitty007@gmail.com>
2022-10-26 10:34:53 +0900

    method #final

--- chapter03/src/main/java/chapter03/T11ArrayUtilTest.java
+++ chapter03/src/main/java/chapter03/T11ArrayUtilTest.java
@@ -3,7 +3,7 @@ package chapter03;
 import java.util.Arrays;
 
 public class T11ArrayUtilTest {
-	//항상 test부터 만들기!!
+	//항상 test코드부터 짜고 실행해보기
 	// static method 익히기
 
 	public static void main(String[] args) {
@@ -14,10 +14,10 @@ public class T11ArrayUtilTest {
 		// Arrays.에 utility 기능이 있음
 		
 		double[] d2 = {0.0, 1.1, 2.2, 3.3, 4.4};
-		T12ArrayUtil.doubleToInt(d2);
+		int[] a2 = T12ArrayUtil.doubleToInt(d2);
 		System.out.println(Arrays.toString(a2));  // [0,1,2,3,4]
 		
-		T12ArrayUtil.concat(new int[]{1, 2, 3, 4}, new int[]{5, 6, 7, 8});
+		int[] a3 = T12ArrayUtil.concat(new int[]{1, 2, 3, 4}, new int[]{5, 6, 7, 8});
 		System.out.println(Arrays.toString(a3));  // 1, 2, 3, 4, 5, 6, 7, 8
 
 	}
