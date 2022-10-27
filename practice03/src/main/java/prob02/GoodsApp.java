10c9315f846299884a37a9e0add0233f5fdbe757
Son-Sumin <goldkitty007@gmail.com>
2022-10-27 09:01:44 +0900

    practice03 #2

--- practice03/src/main/java/prob02/GoodsApp.java
+++ practice03/src/main/java/prob02/GoodsApp.java
@@ -9,19 +9,20 @@ public class GoodsApp {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		//Goods[] goods = new Goods[COUNT_GOODS];  // Goods 만들어야 함
+		Goods[] goods = new Goods[COUNT_GOODS];  // Goods 만들어야 함
 
 		// 상품 입력
 		
 		String line = scanner.nextLine();
 		String[] infos = line.split(" ");
-		//System.out.println(Arrays.toString(infos));
+		System.out.println(Arrays.toString(infos));
 		
 		String name = infos[0];
 		int price = Integer.parseInt(infos[1]);
 		int countStock = Integer.parseInt(infos[2]);
 
 		// 상품 출력
+		//goods.showInfo();
 		
 		// 자원정리
 		scanner.close();
